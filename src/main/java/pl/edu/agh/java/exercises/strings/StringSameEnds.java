14de3d40a7d11279e56d36996d431c2040f8920b
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-03-09 22:01:20 +0100

    StringSameEnds works properly

--- src/main/java/pl/edu/agh/java/exercises/strings/StringSameEnds.java
+++ src/main/java/pl/edu/agh/java/exercises/strings/StringSameEnds.java
@@ -1,16 +1,42 @@
 package pl.edu.agh.java.exercises.strings;
 
+import static java.lang.System.out;
+
 /**
- * Given a string, return the longest substring that appears at both the beginning and end of the string without
- * overlapping. For example, sameEnds("abXab") is "ab".
+ * Given a string, return the longest substring that appears at both the
+ * beginning and end of the string without overlapping. For example,
+ * sameEnds("abXab") is "ab".
  * <p>
- * Dla danego ciągu znaków zwróć najdłuższy podciąg, który występuje na początku i na końcu, bez nachodzenia na siebie.
- * Przykłądowo, sameEnds("abXab") to "ab".
+ * Dla danego ciągu znaków zwróć najdłuższy podciąg, który występuje na początku
+ * i na końcu, bez nachodzenia na siebie. Przykłądowo, sameEnds("abXab") to
+ * "ab".
  *
  * @see http://codingbat.com/prob/p131516
  */
 public class StringSameEnds {
-    public String sameEnds(String string) {
-        throw new UnsupportedOperationException();
-    }
+	public String sameEnds(String string) {
+		// juz mi mozg siadl, bo ostatnie zadanie wiec debug :)
+		byte debug=0;
+		if (debug ==1) out.println("caly string: " + string);
+		String theLongest = "";
+		// lecimy po wszystkich literach
+		for (int i = 0; i < string.length(); i++) {
+			// zaczynamy tworzyc substring
+			String substring = Character.toString(string.charAt(i));
+			if (debug ==1)  out.println("iteracja po wszystkim, pierwsza literka: " + substring);
+			// jesli istnieje,
+			int k = i + 1;
+			while (substring.length() <= (string.length() / 2) && string.indexOf(substring, k) != -1) {
+				if (debug ==1)  out.println("wygenerowany substring: " + substring);
+				if (substring.length() > theLongest.length()) {
+					theLongest = substring;
+				}
+				String letter = Character.toString(string.charAt(k));
+				substring += letter;
+				if (debug ==1)  out.println("wygenerowany substring: " + substring);
+				k++;
+			}
+		}
+		return theLongest;
+	}
 }
